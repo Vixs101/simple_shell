c8377709398a9a69037a4b3b56f097b83aa9fff2
Vixs101 <elijahvix695@gmail.com>
2024-01-01 13:46:08 +0100

    created a header file

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+
+#endif /*MAIN_H*/
